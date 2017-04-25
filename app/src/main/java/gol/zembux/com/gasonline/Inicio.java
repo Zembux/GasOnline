ab9f6cd34b4ef5200d34fc177b6799af282c3303
Luis Enrique Martinez <zembux@MacBook-Pro-de-Luis.local>
2017-04-24 23:48:50 -0500

    Initial commit

---
+++ app/src/main/java/gol/zembux/com/gasonline/Inicio.java
@@ -0,0 +1,14 @@
+package gol.zembux.com.gasonline;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+
+}
