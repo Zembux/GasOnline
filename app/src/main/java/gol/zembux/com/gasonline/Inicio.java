306798965bf858b3183f8d642e60dc31ca281597
zembux <zembux@gmail.com>
2017-04-25 01:11:42 -0500

    1.3

--- app/src/main/java/gol/zembux/com/gasonline/Inicio.java
+++ app/src/main/java/gol/zembux/com/gasonline/Inicio.java
@@ -1,7 +1,12 @@
 package gol.zembux.com.gasonline;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.facebook.AccessToken;
+import com.facebook.login.LoginManager;
 
 public class Inicio extends AppCompatActivity {
 
@@ -9,6 +14,28 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        //Aquí se habilita la sesión de Facebook
+        if (AccessToken.getCurrentAccessToken() == null){
+            goLoginScreen();
+        }
     }
 
+    private void goLoginScreen(){
+        Intent intent = new Intent(this, Login.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    public void logout(View view) {
+        LoginManager.getInstance().logOut();
+        goLoginScreen();
+    }
+/*    public void goRegistra(View view){
+        Intent in = new Intent(this, Almacenamiento.class);
+        startActivity(in);
+    }
+    public void goImagen(View view){
+        Intent inI = new Intent(this, VerImagen.class);
+        startActivity(inI);
+    }*/
 }
---
+++ app/src/main/java/gol/zembux/com/gasonline/Objetos/Firebase_References.java
@@ -0,0 +1,9 @@
+package gol.zembux.com.gasonline.Objetos;
+
+/**
+ * Created by zembux on 25/04/17.
+ */
+
+public class Firebase_References {
+    final public static String GO_REFERENCIA = "gasonline";
+}
